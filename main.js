70b6bb1f55fdc25476d78aa5896b2fe682665c9c
JenGates3255 <Jengates3255@gmail.com>
2014-07-17 20:24:07 -0600

    photos

--- main.js
+++ main.js
@@ -9,133 +9,133 @@ var myItems = [
 		category: 'tops',
 		type:'tank',
 		color:'purple',
-		img:'file:///Users/jgates/Pictures/MIDTERM-PHOTOS/tops/tanks/tank.jpg'
+		img:'MIDTERM-PHOTOS/tops/tanks/tank.jpg'
 	},
 	{
 		name: 'black tank',
 		category: 'tops',
 		type:'tank',
 		color:'black',
-		img:'file:///Users/jgates/Pictures/MIDTERM-PHOTOS/tops/tanks/tank%202.jpg" alt="blackTank'
+		img:'MIDTERM-PHOTOS/tops/tanks/tank%202.jpg" alt="blackTank'
 	},
 	{
 		name: 'black striped',
 		category: 'tops',
 		type:'long-sleeve',
 		color: 'black',
-		img: 'file:///Users/jgates/Pictures/MIDTERM-PHOTOS/tops/longslv/long%20sleeve%20t.jpg'
+		img: 'MIDTERM-PHOTOS/tops/longslv/long%20sleeve%20t.jpg'
 	},
 	{
 		name:'blue slouchie',
 		category: 'tops',
 		type:'long-sleeve',
 		color:'blue',
-		img:'file:///Users/jgates/Pictures/MIDTERM-PHOTOS/tops/longslv/long%20sleeve%20t%202.jpg'
+		img:'MIDTERM-PHOTOS/tops/longslv/long%20sleeve%20t%202.jpg'
 	},
 	{
 		name:'California tee',
 		category: 'tops',
 		type:'short-sleeve',
 		color:'black',
-		img:'file:///Users/jgates/Pictures/MIDTERM-PHOTOS/tops/shortslv/short%20sleeve%20t.jpg'
+		img:'MIDTERM-PHOTOS/tops/shortslv/short%20sleeve%20t.jpg'
 	},
 	{
 		name:'teal short sleeve',
 		category: 'tops',
 		type:'short-sleeve', 
 		color:'blue',
-		img:'file:///Users/jgates/Pictures/MIDTERM-PHOTOS/tops/shortslv/short%20sleeve%20t%202.jpg'
+		img:'MIDTERM-PHOTOS/tops/shortslv/short%20sleeve%20t%202.jpg'
 	},
 	{
 		name: 'black slacks',
 		category:'bottoms',
 		type:'pants',
 		color:'black',
-		img:'file:///Users/jgates/Pictures/MIDTERM-PHOTOS/bottoms/pants/pants.jpg'
+		img:'MIDTERM-PHOTOS/bottoms/pants/pants.jpg'
 	},
 	{
 		name:'khaki slacks',
 		category:'bottoms',
 		type:'pants',
 		color:'beige',
-		img:'file:///Users/jgates/Pictures/MIDTERM-PHOTOS/bottoms/pants/pants%202.jpg'
+		img:'MIDTERM-PHOTOS/bottoms/pants/pants%202.jpg'
 	},
 	{
 		name:'jeans',
 		category:'bottoms',
 		type:'pants',
 		color:'denim',
-		img:'file:///Users/jgates/Pictures/MIDTERM-PHOTOS/bottoms/pants/pants%203.jpg'
+		img:'MIDTERM-PHOTOS/bottoms/pants/pants%203.jpg'
 	},
 	{
 		name:'jean shorts',
 		category:'bottoms',
 		type:'shorts',
 		color:'denim',
-		img:'file:///Users/jgates/Pictures/MIDTERM-PHOTOS/bottoms/shorts/shorts.jpg'
+		img:'MIDTERM-PHOTOS/bottoms/shorts/shorts.jpg'
 	},
 	{
 		name: 'black mini skirt',
 		category:'bottoms',
 		type:'skirts',
 		color:'black',
-		img:'file:///Users/jgates/Pictures/MIDTERM-PHOTOS/bottoms/skirts/skirt.jpg'
+		img:'MIDTERM-PHOTOS/bottoms/skirts/skirt.jpg'
 	},
 	{
 		name:'white pencil skirt',
 		category:'bottoms',
 		type:'skirts',
 		color:'white',
-		img:'file:///Users/jgates/Pictures/MIDTERM-PHOTOS/bottoms/skirts/skirt%203.jpg'
+		img:'MIDTERM-PHOTOS/bottoms/skirts/skirt%203.jpg'
 	},
 	{
 		name:'black skater skirt',
 		category:'bottoms',
 		type:'skirts',
 		color:'black',
-		img:'file:///Users/jgates/Pictures/MIDTERM-PHOTOS/bottoms/skirts/skirt%202.jpg'
+		img:'MIDTERM-PHOTOS/bottoms/skirts/skirt%202.jpg'
 	},
 	{
 		name:'pink maxi',
 		category:'dresses',
 		type: 'long-dress',
 		color:'pink',
-		img:'file:///Users/jgates/Pictures/MIDTERM-PHOTOS/dresses/long%20/long%20dress.jpg'
+		img:'MIDTERM-PHOTOS/dresses/long%20/long%20dress.jpg'
 	},
 	{
 		name:'white maxi',
 		category:'dresses',
 		type:'long-dress',
 		color:'white',
-		img:'file:///Users/jgates/Pictures/MIDTERM-PHOTOS/dresses/long%20/long%20dress%202.jpg'
+		img:'MIDTERM-PHOTOS/dresses/long%20/long%20dress%202.jpg'
 	},
 	{
 		name:'floral dress',
 		category:'dresses',
 		type:'short-dress',
 		color: 'red',
-		img:'file:///Users/jgates/Pictures/MIDTERM-PHOTOS/dresses/short/short%20dress%204.jpg'
+		img:'MIDTERM-PHOTOS/dresses/short/short%20dress%204.jpg'
 	},
 	{
 		name:'pink short dress',
 		category:'dresses',
 		type:'short-dress',
 		color:'pink',
-		img:'file:///Users/jgates/Pictures/MIDTERM-PHOTOS/dresses/short/short%20dress.jpg'
+		img:'MIDTERM-PHOTOS/dresses/short/short%20dress.jpg'
 	},
 	{
 		name:'white short dress',
 		category:'dresses',
 		type:'short-dress',
 		color:'white',
-		img:'file:///Users/jgates/Pictures/MIDTERM-PHOTOS/dresses/short/short%20dress%203.jpg'
+		img:'MIDTERM-PHOTOS/dresses/short/short%20dress%203.jpg'
 	},
 	{
 		name:'black short dress',
 		category:'dresses',
 		type:'short-dress',
 		color:'black',
-		img:'file:///Users/jgates/Pictures/MIDTERM-PHOTOS/dresses/short/short%20dress%202.jpg'
+		img:'MIDTERM-PHOTOS/dresses/short/short%20dress%202.jpg'
 	},
 ];
 
